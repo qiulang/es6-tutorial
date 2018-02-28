d70da3121f44a486c64cce752043880ce18c6410
qiulang@macbook3 <qiulang2000@126.com>
2018-02-28 17:03:28 +0800

    编译多个文件

---
+++ build/app.bundle.js
@@ -0,0 +1,154 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/main.js":
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _mortgage = __webpack_require__(/*! ./mortgage */ "./js/mortgage.js");
+
+document.getElementById('calcBtn').addEventListener('click', function () {
+    var principal = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
+
+    var _calculateAmortizatio = (0, _mortgage.calculateAmortization)(principal, years, rate),
+        monthlyPayment = _calculateAmortizatio.monthlyPayment,
+        monthlyRate = _calculateAmortizatio.monthlyRate,
+        amortization = _calculateAmortizatio.amortization;
+
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+    //amortization.forEach(month => console.log(month));
+    var html = "";
+    amortization.forEach(function (year, index) {
+        return html += '\n        <tr>\n            <td>' + (index + 1) + '</td>\n            <td class="currency">' + Math.round(year.principalY) + '</td>\n            <td class="stretch">\n                <div class="flex">\n                    <div class="bar principal"\n                         style="flex:' + year.principalY + ';-webkit-flex:' + year.principalY + '">\n                    </div>\n                    <div class="bar interest"\n                         style="flex:' + year.interestY + ';-webkit-flex:' + year.interestY + '">\n                    </div>\n                </div>\n            </td>\n            <td class="currency left">' + Math.round(year.interestY) + '</td>\n            <td class="currency">' + Math.round(year.balance) + '</td>\n        </tr>\n    ';
+    });
+    document.getElementById("amortization").innerHTML = html;
+});
+
+/***/ }),
+
+/***/ "./js/mortgage.js":
+/*!************************!*\
+  !*** ./js/mortgage.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var calculateMonthlyPayment = exports.calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+    if (rate) {
+        var monthlyRate = rate / 100 / 12;
+    }
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
+    return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
+};
+
+var calculateAmortization = exports.calculateAmortization = function calculateAmortization(principal, years, rate) {
+    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
+        monthlyRate = _calculateMonthlyPaym.monthlyRate,
+        monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
+
+    var balance = principal;
+    var amortization = [];
+    for (var y = 0; y < years; y++) {
+        var interestY = 0; //Interest payment for year y
+        var principalY = 0; //Principal payment for year y
+        for (var m = 0; m < 12; m++) {
+            var interestM = balance * monthlyRate; //Interest payment for month m
+            var principalM = monthlyPayment - interestM; //Principal payment for month m
+            interestY = interestY + interestM;
+            principalY = principalY + principalM;
+            balance = balance - principalM;
+        }
+        amortization.push({ principalY: principalY, interestY: interestY, balance: balance });
+    }
+    return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
+};
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=app.bundle.js.map
