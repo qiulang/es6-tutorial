caad5605dde6bd242d47f641cf0c185e149e642c
qiulang@macbook3 <qiulang2000@126.com>
2018-02-28 15:19:22 +0800

    clear cache test

---
+++ build/main.bundle.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+    if (rate) {
+        var monthlyRate = rate / 100 / 12;
+    }
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
+    return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
+};
+
+document.getElementById('calcBtn').addEventListener('click', function () {
+    var principal = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
+
+    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
+        monthlyPayment = _calculateMonthlyPaym.monthlyPayment,
+        monthlyRate = _calculateMonthlyPaym.monthlyRate;
+
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+});
