045b3d9b6758e424f182947d975f22bc114498f3
qiulang@macbook3 <qiulang2000@126.com>
2018-02-28 16:21:56 +0800

    只使用babel能编译module吗？

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
-let calculateMonthlyPayment = (principal, years, rate) => {
-    if (rate) {
-        var monthlyRate = rate / 100 / 12;
-    }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return {principal, years, rate, monthlyPayment, monthlyRate};
-};
-
-let calculateAmortization = (principal, years, rate) => {
-    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate);
-    let balance = principal;
-    let amortization = [];
-    for (let y=0; y<years; y++) {
-        let interestY = 0;  //Interest payment for year y
-        let principalY = 0; //Principal payment for year y
-        for (let m=0; m<12; m++) {
-            let interestM = balance * monthlyRate;       //Interest payment for month m
-            let principalM = monthlyPayment - interestM; //Principal payment for month m
-            interestY = interestY + interestM;
-            principalY = principalY + principalM;
-            balance = balance - principalM;
-        }
-        amortization.push({principalY, interestY, balance});
-    }
-    return {monthlyPayment, monthlyRate, amortization};
-};
+import {calculateMonthlyPayment,calculateAmortization} from './mortgage';
 
 document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
